f826154edb18baeb4ca5b70409d07157e124ac09
Arunangshu-Das <darunangshu2002@gmail.com>
2024-01-19 12:02:17 +0530

    C# updated

---
+++ PracticeApp1/PracticeApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApp1
+{
+    abstract class cloud
+    {
+        public enum Level
+        {
+            low,
+            medium,
+            high,
+        }
+        public string compute = null;
+        public string Compute
+        {
+            get;
+            set;
+        }
+        public virtual string getname()
+        {
+            return "hello from cloud";
+        }
+
+        public virtual void getlevel()
+        {
+            Console.WriteLine(Level.medium);
+        }
+
+        public abstract string getSpeed();
+    }
+
+    class aws: cloud
+    {
+        public string compute;
+        public string Compute
+        {
+            set;
+            get;
+        }
+        public override string getname()
+        {
+            return "hello from AWS";
+        }
+
+        public override string getSpeed()
+        {
+            return "99% uptime";
+        }
+        public override void getlevel()
+        {
+            Console.WriteLine(Level.high);
+        }
+    }
+
+    class gcloud : cloud
+    {
+        public string compute;
+        public string Compute
+        {
+            set;
+            get;
+        }
+        public override string getname()
+        {
+            return "hello from gcloud";
+        }
+
+        public override string getSpeed()
+        {
+            return "95% uptime";
+        }
+        public override void getlevel()
+        {
+            Console.WriteLine(Level.medium);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cloud cloud = new aws();
+            cloud.Compute = "EC2";
+            Console.WriteLine(cloud.Compute);
+            Console.WriteLine(cloud.getname());
+            Console.WriteLine(cloud.getSpeed());
+            cloud.getlevel();
+            cloud=new gcloud();
+            cloud.Compute = "NanoCloud";
+            Console.WriteLine(cloud.Compute);
+            Console.WriteLine(cloud.getname());
+            Console.WriteLine(cloud.getSpeed());
+            cloud.getlevel();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ project1/project1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "file.txt";
+            do
+            {
+                Console.WriteLine("Enter data to add file");
+                string inputData= Console.ReadLine();
+                using (StreamWriter writer = new StreamWriter(file,true))
+                {
+                    writer.WriteLine(inputData);
+                }
+                Console.WriteLine("Do you want to add more data y/n");
+                string choice=Console.ReadLine().ToLower();
+                if(choice=="n" || choice == "no")
+                {
+
+                    if (File.Exists(file))
+                    {
+                        Console.WriteLine();
+                        string[] lines = File.ReadAllLines(file);
+
+                        foreach (string ln in lines)
+                            Console.WriteLine(ln);
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("Do you want to add more data y/n");
+                    choice = Console.ReadLine().ToLower();
+                    if(choice=="n" || choice == "no")
+                    {
+                        break;
+                    }
+                }
+            }
+            while (true);
+        }
+    }
+}
