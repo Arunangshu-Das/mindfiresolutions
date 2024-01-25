5a506b5eb91bd0650d90148d07c7494e1a385f83
Arunangshu-Das <darunangshu2002@gmail.com>
2024-01-25 17:34:06 +0530

    C# reflection  function practice

--- DatatableShowData/DatatableShowData/Program.cs
+++ DatatableShowData/DatatableShowData/Program.cs
@@ -11,22 +11,24 @@ namespace DatatableShowData
 {
     public class Student
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email{ get; set; }
-        public string Salary { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email{ get; set; }
+        public string salary { get; set; }
+
+        public Student() { }    
 
         public Student(object id, object name, object email, object salary)
         {
-            Id = (int) id;
-            Name = (string) name;
-            Email = (string) email;
-            Salary = (string) salary;
+            id = (int) id;
+            name = (string) name;
+            email = (string) email;
+            salary = (string) salary;
         }
 
         public void ToString()
         {
-            Console.WriteLine("Id " + Id + " Name " + Name + " Email " + Email + " Salary " + Salary);
+            Console.WriteLine("Id " + id + " Name " + name + " Email " + email + " Salary " + salary);
         }
     }
     internal class Program
@@ -39,20 +41,70 @@ namespace DatatableShowData
                 SqlDataAdapter adapter = new SqlDataAdapter(query,conn);
                 
                 DataTable dataTable = new DataTable();  
-                conn.Open();
                 adapter.Fill(dataTable);
-                conn.Close();
-                List<Student> students = new List<Student>();
-                foreach(DataRow row in dataTable.Rows) 
-                {
-                    students.Add(new Student(row["id"], row["name"], row["email"], row["salary"]));
-                }
+                List<Student> students = null;
+                students = dataTable.toList<Student>();
+                //foreach(DataRow row in dataTable.Rows) 
+                //{
+                //    students.Add(new Student(row["id"], row["name"], row["email"], row["salary"]));
+                //}
+
+
+
                 for(int i = 0; i < students.Count; i++)
                 {
                     students[i].ToString();
                 }
             }
             Console.ReadLine();
+        }
+
+        static List<T> ConvertList<T>(DataTable dataTable) where T : new()
+        {
+            List<T> newList= new List<T>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T t= new T();
+                foreach(var property in typeof(T).GetProperties())
+                {
+                    if (dataTable.Columns.Contains(property.Name.ToLower()))
+                    {
+                        var value = Convert.ChangeType(row[property.Name], property.PropertyType);
+                        property.SetValue(t, value);
+                    }
+                }
+                newList.Add(t);
+            }
+
+            return newList;
+        }
+
+        
+    }
+
+
+    public static class Listify
+    {
+        public static List<T> toList<T>(this DataTable dataTable) where T : class, new()
+        {
+            List<T> newList = new List<T>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T t = new T();
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    if (dataTable.Columns.Contains(property.Name.ToLower()))
+                    {
+                        var value = Convert.ChangeType(row[property.Name], property.PropertyType);
+                        property.SetValue(t, value);
+                    }
+                }
+                newList.Add(t);
+            }
+
+            return newList;
         }
     }
 }
