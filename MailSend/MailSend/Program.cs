f485f36d65652d1527bbbb34cae13099d132afc8
Arunangshu-Das <darunangshu2002@gmail.com>
2024-01-25 17:44:54 +0530

    C# mail assignment

--- MailSend/MailSend/Program.cs
+++ MailSend/MailSend/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace MailSend
 {
@@ -10,6 +13,14 @@ namespace MailSend
     {
         static void Main(string[] args)
         {
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("c9aaf2b4589b0c", "54272606303edc"),
+                EnableSsl = false
+            };
+            client.Send("arunangshud@mindfiresolutions.com", "sdasdsa@sadasd.com", "Hello world", "testbody");
+            Console.WriteLine("Sent");
+            Console.ReadLine();
         }
     }
 }
